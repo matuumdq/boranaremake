0229afaa9c1c25a39b55b2dc6e439d7d9f555f5e
matuumdq <salinasmateo@outlook.com>
2023-06-05 16:55:16 -0300

    fix: :zap: carousel

--- src/components/FoundUs.jsx
+++ src/components/FoundUs.jsx
@@ -2,36 +2,36 @@ import AliceCarousel from "react-alice-carousel";
 import "react-alice-carousel/lib/alice-carousel.css";
 const FoundUs = () => {
 	const responsive = {
-		0: { items: 1 },
-		568: { items: 2 },
-		1024: { items: 3 },
+		0: { items: 2 },
+		568: { items: 3 },
+		1024: { items: 4 },
 	};
 	const items = [
-		<div className="item w-full mx-auto" data-value="1" key="1">
+		<div className="item w-full" data-value="1" key="1">
 			<img
 				src="https://res.cloudinary.com/dnqmez68n/image/upload/v1685735474/ninio_qvqpku.png"
 				alt=""
 			/>
 		</div>,
-		<div className="item w-full mx-auto" data-value="2" key="2">
+		<div className="item w-full" data-value="2" key="2">
 			<img
 				src="https://res.cloudinary.com/dnqmez68n/image/upload/v1685735474/casa_dgs2f3.png"
 				alt=""
 			/>
 		</div>,
-		<div className="item w-full mx-auto" data-value="3" key="3">
+		<div className="item w-full" data-value="3" key="3">
 			<img
 				src="https://res.cloudinary.com/dnqmez68n/image/upload/v1685735474/bonjus_ofwhca.png"
 				alt=""
 			/>
 		</div>,
-		<div className="item w-full mx-auto" data-value="4" key="4">
+		<div className="item w-full" data-value="4" key="4">
 			<img
 				src="https://res.cloudinary.com/dnqmez68n/image/upload/v1685735474/gato_ythnaa.png"
 				alt=""
 			/>
 		</div>,
-		<div className="item w-full mx-auto" data-value="5" key="5">
+		<div className="item w-full" data-value="5" key="5">
 			<img
 				src="https://res.cloudinary.com/dnqmez68n/image/upload/v1685735474/barista_thweox.png"
 				alt=""
@@ -48,7 +48,7 @@ const FoundUs = () => {
 				autoPlay
 				autoPlayStrategy="default"
 				animationDuration="1500"
-				paddingLeft={80}
+				// paddingLeft={80}
 				disableDotsControls
 				disableButtonsControls
 			/>
